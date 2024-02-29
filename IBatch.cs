a8bdcba80c919b147d4e0e80fb1812606f9be54e
gator7 <69212474+jblackwell7@users.noreply.github.com>
2024-02-29 11:29:45 -0600

    Addenda Record Check

--- IBatch.cs
+++ IBatch.cs
@@ -5,7 +5,7 @@ namespace NACHAParser
         #region Methods
 
         void ProcessBatchHeader(string line, int lineNumber, StandardEntryClassCode sec);
-        void ProcessEntryDetail(string line);
+        void ProcessEntryDetail(string line, string nextLine, int lineNumber);
         void ProcessAddenda(string line);
         void ProcessBatchControl(string line, Root root);
 
