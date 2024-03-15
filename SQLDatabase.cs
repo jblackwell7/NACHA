aaf3b7094ec87adf7c50d25f7d536f3f9201084d
gator7 <69212474+jblackwell7@users.noreply.github.com>
2024-03-15 14:30:34 -0500

    ACH-21 Update Insert Methods and GetStoredProc method

--- SQLDatabase.cs
+++ SQLDatabase.cs
@@ -55,7 +55,7 @@ namespace NACHAParser
                     {
                         foreach (var addenda in entry.AddendaRecord)
                         {
-                            SQLInsertAddendaRecord(addenda, batch, connectionString);
+                            SQLInsertAddendaRecord(addenda, batch, batch.BatchHeader.SECCode, connectionString);
                         }
                     }
                 }
@@ -65,21 +65,95 @@ namespace NACHAParser
         }
         private static void SQLInsertFileHeaderRecord(FileHeaderRecord fh, string connectionString)
         {
+            string procName = GetStoredProc(fh.RecType);
+            SqlParameter[] parameters = {
+                    new SqlParameter("@FileheaderId", fh.FileheaderId),
+                    new SqlParameter("@RecordType", (int)fh.RecType),
+                    new SqlParameter("@PriorityCode", fh.PriorityCode),
+                    new SqlParameter("@ImmedDestination", fh.ImmedDestination),
+                    new SqlParameter("@ImmedOrigin", fh.ImmedOrigin),
+                    new SqlParameter("@FileCreationDate", fh.FileCreationDate),
+                    new SqlParameter("@FileCreationTime", fh.FileCreationTime),
+                    new SqlParameter("@FileIDModifier", fh.FileIDModifier),
+                    new SqlParameter("@RecSize", fh.RecSize),
+                    new SqlParameter("@BlockingFactor", fh.BlockingFactor),
+                    new SqlParameter("@FormatCode", fh.FormatCode),
+                    new SqlParameter("@ImmedDestinationName", fh.ImmedDestinationName),
+                    new SqlParameter("@ImmedOriginName", fh.ImmedOriginName),
+                    new SqlParameter("@RefCode", fh.RefCode)
+                    };
+            ExecuteProc(connectionString, procName, parameters);
         }
         private static void SQLInsertBatchHeaderRecord(BatchHeaderRecord bh, Root root, string connectionString)
         {
+            string procName = GetStoredProc(bh.RecType);
+            SqlParameter[] parameters = {
+                new SqlParameter("@BatchHeaderId", bh.BchHeaderId),
+                new SqlParameter("@FileheaderId", root.FileContents.AchFile.FileHeader.FileheaderId),
+                new SqlParameter("@RecordType", (int)bh.RecType),
+                new SqlParameter("@ServiceClassCode", (int)bh.ServiceClassCode),
+                new SqlParameter("@CompanyName", bh.CoName),
+                new SqlParameter("@CompanyDiscretionaryData", bh.CoDiscretionaryData),
+                new SqlParameter("@CompanyID", bh.CoId),
+                new SqlParameter("@StandardEntryClassCode", bh.SECCode.ToString()),
+                new SqlParameter("@CompanyEntryDescription", bh.CoEntDescription),
+                new SqlParameter("@CompanyDescriptiveDate", bh.CoDescriptiveDate),
+                new SqlParameter("@EffectiveEntryDate", bh.EffectiveEntDate),
+                new SqlParameter("@SettlementDate", bh.SettlementDate),
+                new SqlParameter("@OriginatorStatusCode", bh.OriginatorStatusCode),
+                new SqlParameter("@OriginatingDFIId", bh.OriginatingDFIId),
+                new SqlParameter("@BatchNumber", bh.BchNum),
+            };
+            ExecuteProc(connectionString, procName, parameters);
         }
         private static void SQLInsertEntryDetailRecord(EntryDetailRecord ed, Batch batch, StandardEntryClassCode sec, string connectionString)
         {
+            SqlParameter[] parameters = GetEntryDetailParameters(ed, batch, sec);
+            string procName = GetStoredProc(ed.RecType);
+            ExecuteProc(connectionString, procName, parameters);
         }
-        private static void SQLInsertAddendaRecord(Addenda ad, Batch batch, string connectionString)
+        private static void SQLInsertAddendaRecord(Addenda ad, Batch batch, StandardEntryClassCode sec, string connectionString)
         {
+            SqlParameter[] parameters = GetAddendaParameters(ad, batch, sec);
+            string procName = GetStoredProc(ad.RecType);
+            ExecuteProc(connectionString, procName, parameters);
         }
         private static void SQLInsertBatchControlRecord(BatchControlRecord bc, Batch batch, string connectionString)
         {
+            string procName = GetStoredProc(bc.RecType);
+            SqlParameter[] parameters = {
+                    new SqlParameter("@BatchControlId", bc.BchControlId),
+                    new SqlParameter("@BatchHeaderId", batch.BatchHeader.BchHeaderId),
+                    new SqlParameter("@RecordType", (int)bc.RecType),
+                    new SqlParameter("@ServiceClassCode", (int)bc.ServiceClassCode),
+                    new SqlParameter("@EntryAddendaCount", bc.EntAddendaCnt),
+                    new SqlParameter("@EntryHash", bc.EntHash),
+                    new SqlParameter("@TotalDebitAmount", bc.TotBchDrEntAmt),
+                    new SqlParameter("@TotalCreditAmount", bc.TotBchCrEntAmt),
+                    new SqlParameter("@CompanyID", bc.CoId),
+                    new SqlParameter("@MessageAuthenticationCode", bc.MsgAuthCode),
+                    new SqlParameter("@Reserved", bc.Reserved),
+                    new SqlParameter("@OriginatingDFIId", bc.OriginatingDFIId),
+                    new SqlParameter("@BatchNumber", bc.BchNum),
+            };
+            ExecuteProc(connectionString, procName, parameters);
         }
         private static void SQLInsertFileControlRecord(FileControlRecord fc, FileHeaderRecord fh, string connectionString)
-        {
+        {string procName = GetStoredProc(fc.RecType);
+            SqlParameter[] parameters = {
+                new SqlParameter("@FileControlId", fc.FileControlId),
+                new SqlParameter("@FileheaderId", fh.FileheaderId),
+                new SqlParameter("@RecordType", (int)fc.RecType),
+                new SqlParameter("@BatchCount", fc.BchCnt),
+                new SqlParameter("@BlockCount", fc.BlockCnt),
+                new SqlParameter("@EntryAddendaCount", fc.EntAddendaCnt),
+                new SqlParameter("@EntryHash", fc.EntHash),
+                new SqlParameter("@TotalDebitAmount", fc.TotFileDrEntAmt),
+                new SqlParameter("@TotalCreditAmount", fc.TotFileCrEntAmt),
+                new SqlParameter("@Reserved", fc.Reserved),
+            };
+
+            ExecuteProc(connectionString, "sp_InsertFileControlRecord", parameters);
         }
         private static SqlParameter[] GetEntryDetailParameters(EntryDetailRecord ed, Batch batch, StandardEntryClassCode sec)
         {
@@ -89,9 +163,29 @@ namespace NACHAParser
         {
             return null;
         }
-        private static string GetStoredProcs()
+        private static string GetStoredProc(RecordType recType)
         {
-            return null;
+            //TODO: ACH-22 Add StandardEntryClassCode logic
+            //TODO: ACH-23 RecordType.ed and AdTypeCode for RecordType.ad
+            //TODO: ACH-24 Add stored procs for to support ACH-23
+
+            switch (recType)
+            {
+                case RecordType.fh:
+                    return "sp_InsertFileHeaderRecord";
+                case RecordType.bh:
+                    return "sp_InsertBatchHeaderRecord";
+                case RecordType.ed:
+                    return "sp_InsertEntryDetailRecord";
+                case RecordType.ad:
+                    return "sp_InsertAddendaRecord";
+                case RecordType.bc:
+                    return "sp_InsertBatchControlRecord";
+                case RecordType.fc:
+                    return "sp_InsertFileControlRecord";
+                default:
+                    throw new InvalidOperationException($"There is not a valid Stored Procedure for Record Type '{recType}' is not supported");
+            }
         }
     }
 }
