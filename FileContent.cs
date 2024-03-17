0ecc99fe1453a3e3af68812ec5afdc95fabf863d
gator7 <69212474+jblackwell7@users.noreply.github.com>
2024-03-17 18:39:19 -0500

    ACH-29 Update ACHFile class

--- FileContent.cs
+++ FileContent.cs
@@ -4,18 +4,23 @@ namespace NACHAParser
     public class Root
     {
         #region Properties
+
         [JsonProperty("fileContents")]
         public FileContents? FileContents { get; set; }
+
         #endregion
     }
     public class FileContents
     {
         #region Properties
+
         [JsonProperty("fileId")]
         public string FileId { get; set; } = string.Empty;
         [JsonProperty("achFile")]
         public ACHFile? AchFile { get; set; }
+
         #endregion
+
         #region Constructors
         public FileContents()
         {
@@ -26,13 +31,86 @@ namespace NACHAParser
     }
     public class ACHFile
     {
+        #region Fields
+
+        private Batch? _currentBatch;
+        private List<Batch>? _batches = new List<Batch>();
+
+        #endregion
+
         #region Properties
+
+        [JsonProperty("achFileId")]
+        public string AchFileId { get; set; } = string.Empty;
+
         [JsonProperty("fileHeader")]
         public FileHeaderRecord? FileHeader { get; set; }
+
         [JsonProperty("batches")]
-        public List<Batch>? Batches { get; set; }
+        public IReadOnlyList<Batch>? Batches
+        {
+            get
+            {
+                return _batches;
+            }
+        }
+
         [JsonProperty("fileControl")]
         public FileControlRecord? FileControl { get; set; }
+
+        public Batch? CurrentBatch
+        {
+            get
+            {
+                return _currentBatch;
+            }
+            private set
+            {
+                _currentBatch = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public ACHFile()
+        {
+            AchFileId = Guid.NewGuid().ToString();
+            Console.WriteLine($"AchFileId: '{AchFileId}'");
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void AddBatch(Batch batch)
+        {
+            _batches.Add(batch);
+        }
+        public void SetCurrentBatch(Batch batch)
+        {
+            if (_batches.Contains(batch))
+            {
+                CurrentBatch = batch;
+            }
+            else
+            {
+                throw new Exception("Batch not found in list of batches");
+            }
+        }
+        public Batch GetCurrentBatch()
+        {
+            if (CurrentBatch == null)
+            {
+                throw new Exception("CurrentBatch is null");
+            }
+            else
+            {
+                return CurrentBatch;
+            }
+        }
+
         #endregion
     }
 }
